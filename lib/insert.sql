8081b9755e10a56e15c68774b240eea99280b96c
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-11-07 10:22:45 -0700

    done

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'Black', 'Good', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 15, 'M', 'Brown', 'Bad', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 10, 'F', 'Red', 'Good', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Seargeant Brown', 23, 'M', 'Purple', 'Rough around the Edges', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 18, 'F', 'Orange', 'Cheery', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 35, 'M', 'Green', 'Grumpy', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 21, 'F', 'Brown', 'Mean', TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, 'M', 'Black', 'Jumpy', TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
